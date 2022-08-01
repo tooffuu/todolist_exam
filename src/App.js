1a6cd197eeb1a3fde91433a2b5f81973050655c2
unknown <thdud8866@naver.com>
2022-08-01 16:19:37 +0900

    react-todolist check

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const onChange = (e) => {
+    setTodo(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todo === "") {
+      alert("1글자 이상 입력해주세요");
+      return;
+    }
+
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+    alert("등록됨");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>*할 일*</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          value={todo}
+          placeholder="할 일을 적어주세요"
+        />
+        <button> 등록 </button>
+      </form>
+
+      <button
+        onClick={() => {
+          console.log(todos);
+        }}
+      >
+        check
+      </button>
     </div>
   );
 }
